ce839a41adabf120da3c65f27c3a1ec3153604bf
makekryl <makekryl@gmail.com>
2025-06-17 20:36:01 +0000

    Fix GetNodeAt for SelectorExpr

--- src/core/include/utils/ASTUtils.h
+++ src/core/include/utils/ASTUtils.h
@@ -6,6 +6,13 @@
 namespace vanadium::core::ast {
 namespace utils {
 
+inline const Node* TraverseSelectorExpressionStart(const nodes::SelectorExpr* se) {
+  while (se->x->nkind == core::ast::NodeKind::SelectorExpr) {
+    se = se->x->As<core::ast::nodes::SelectorExpr>();
+  }
+  return se->x;
+}
+
 inline SourceFile* SourceFileOf(const Node* n) {
   while (n->nkind != NodeKind::Root) {
     n = n->parent;
@@ -13,23 +20,25 @@ inline SourceFile* SourceFileOf(const Node* n) {
   return n->As<RootNode>()->file;
 }
 
+// TODO: utilize binary search assuming node lists are sorted
 inline const Node* GetNodeAt(const AST& ast, pos_t pos) {
   const Node* candidate;
   ast.root->Accept([&](const Node* n) {
-    if (n->nrange.Contains(pos)) {
+    bool pass;
+    if (n->nkind == NodeKind::SelectorExpr) {
+      const auto* head = TraverseSelectorExpressionStart(n->As<nodes::SelectorExpr>());
+      pass = head->nrange.begin <= pos && pos <= n->nrange.end;
+    } else {
+      pass = n->nrange.Contains(pos);
+    }
+
+    if (pass) {
       candidate = n;
       return true;
     }
     return false;
   });
   return candidate;
-}
-
-inline const Node* TraverseSelectorExpressionStart(const nodes::SelectorExpr* se) {
-  while (se->x->nkind == core::ast::NodeKind::SelectorExpr) {
-    se = se->x->As<core::ast::nodes::SelectorExpr>();
-  }
-  return se->x;
 }
 
 }  // namespace utils
