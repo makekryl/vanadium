52597bb1a011fcf4db482bf2e1c23f2cb6c0bc88
makekryl <makekryl@gmail.com>
2025-07-24 21:46:34 +0000

    scopednodevisitor: fix templatedecl typecheck

--- src/core/include/ScopedNodeVisitor.h
+++ src/core/include/ScopedNodeVisitor.h
@@ -11,19 +11,21 @@ void InspectScope(const Scope* scope, std::invocable<const Scope*> auto set_scop
                   std::predicate<const ast::Node*> auto visit, const Scope* prev_scope = nullptr) {
   set_scope(scope);
 
-  auto [child_it, children_it_end] = std::make_pair(scope->GetChildren().cbegin(), scope->GetChildren().cend());
-  scope->Container()->Accept([&](const ast::Node* n) {
-    // assuming scope children are sorted
-    if (child_it != children_it_end) {
-      const auto* child = *child_it;
-      if (child->Container()->nrange.Contains(n->nrange)) {
-        ++child_it;
-        InspectScope(child, set_scope, visit, scope);
-        return false;
+  if (visit(scope->Container())) {
+    auto [child_it, children_it_end] = std::make_pair(scope->GetChildren().cbegin(), scope->GetChildren().cend());
+    scope->Container()->Accept([&](const ast::Node* n) {
+      // assuming scope children are sorted
+      if (child_it != children_it_end) {
+        const auto* child = *child_it;
+        if (child->Container()->nrange.Contains(n->nrange)) {
+          ++child_it;
+          InspectScope(child, set_scope, visit, scope);
+          return false;
+        }
       }
-    }
-    return visit(n);
-  });
+      return visit(n);
+    });
+  }
 
   set_scope(prev_scope);
 }
