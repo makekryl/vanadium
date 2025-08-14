d54b86d7a7ea640c0b646d8a5fb7a01d98521f32
makekryl <makekryl@gmail.com>
2025-08-14 23:01:09 +0000

    bump glaze

--- cmake/external/glaze.cmake
+++ cmake/external/glaze.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
-  glaze          # 5.5.4
+  glaze          # 5.5.5
   GIT_REPOSITORY https://github.com/stephenberry/glaze.git
-  GIT_TAG        5a58d7936ece70eeb64cc30aa82e9e83ae9c05bb
+  GIT_TAG        5c24f146326580da68047c2180e9570b9c04b82e
   GIT_SHALLOW 1
 )
 FetchContent_MakeAvailable(glaze)
