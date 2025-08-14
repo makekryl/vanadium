64ff3e961c0ffb8c4369b6786767225994f43258
makekryl <makekryl@gmail.com>
2025-08-14 23:04:34 +0000

    cleanup

--- lib/common/include/FunctionRef.h
+++ lib/common/include/FunctionRef.h
@@ -11,35 +11,35 @@ class FunctionRef;
 template <typename TReturn, typename... TParams>
 class FunctionRef<TReturn(TParams...)> final {
  private:
-  using TStub = TReturn (*)(void* instance, TParams...);
+  using TSignature = TReturn (*)(void* instance, TParams...);
 
  public:
   constexpr FunctionRef(std::nullptr_t) {};
 
   template <typename Func>
-  FunctionRef(const Func& func) : FunctionRef((void*)(&func), lambda_stub<Func>) {}
+  FunctionRef(const Func& func) : FunctionRef((void*)(&func), invoke_lambda<Func>) {}
 
   constexpr operator bool() const noexcept {
-    return stub_ != nullptr;
+    return vptr_ != nullptr;
   }
 
   TReturn operator()(TParams... arg) const {
-    return (*stub_)(instance_, arg...);
+    return (*vptr_)(instance_, arg...);
   }
 
   template <class T, TReturn (T::*TMethod)(TParams...)>
   constexpr static FunctionRef create(T* instance) {
-    return FunctionRef(instance, method_stub<T, TMethod>);
+    return FunctionRef(instance, invoke_method<T, TMethod>);
   }
 
   template <class T, TReturn (T::*TMethod)(TParams...) const>
   constexpr static FunctionRef create(T const* instance) {
-    return FunctionRef(const_cast<T*>(instance), const_method_stub<T, TMethod>);
+    return FunctionRef(const_cast<T*>(instance), invoke_const_method<T, TMethod>);
   }
 
   template <TReturn (*TMethod)(TParams...)>
   constexpr static FunctionRef create() {
-    return FunctionRef(nullptr, function_stub<TMethod>);
+    return FunctionRef(nullptr, invoke_fn<TMethod>);
   }
 
   template <typename Func>
@@ -48,36 +48,33 @@ class FunctionRef<TReturn(TParams...)> final {
   }
 
  private:
-  constexpr FunctionRef(void* instance, TStub stub) {
-    instance_ = instance;
-    stub_ = stub;
-  }
+  constexpr FunctionRef(void* instance, TSignature vptr) : instance_(instance), vptr_(vptr) {}
 
   template <TReturn (*TMethod)(TParams...)>
-  constexpr static TReturn function_stub(void*, TParams... params) {
+  constexpr static TReturn invoke_fn(void*, TParams... params) {
     return (TMethod)(std::forward<TParams>(params)...);
   }
 
   template <typename Func>
-  constexpr static TReturn lambda_stub(void* instance, TParams... params) {
+  constexpr static TReturn invoke_lambda(void* instance, TParams... params) {
     Func* p = static_cast<Func*>(instance);
     return (p->operator())(std::forward<TParams>(params)...);
   }
 
   template <class T, TReturn (T::*TMethod)(TParams...)>
-  constexpr static TReturn method_stub(void* instance, TParams... params) {
+  constexpr static TReturn invoke_method(void* instance, TParams... params) {
     auto* p = static_cast<T*>(instance);
     return (p->*TMethod)(std::forward<TParams>(params)...);
   }
 
   template <class T, TReturn (T::*TMethod)(TParams...) const>
-  constexpr static TReturn const_method_stub(void* instance, TParams... params) {
+  constexpr static TReturn invoke_const_method(void* instance, TParams... params) {
     auto* const p = static_cast<T*>(instance);
     return (p->*TMethod)(std::forward<TParams>(params)...);
   }
 
   void* instance_ = nullptr;
-  TStub stub_ = nullptr;
+  TSignature vptr_ = nullptr;
 };
 
 template <typename... TParams>
