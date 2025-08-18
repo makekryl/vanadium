cb30591cdc5db8925d7f0d67a52f80fc0cc7735a
makekryl <makekryl@gmail.com>
2025-08-18 22:25:25 +0000

    NoUnusedVars: exclude external funcs

--- src/lint/src/rules/NoUnusedVars.cpp
+++ src/lint/src/rules/NoUnusedVars.cpp
@@ -99,9 +99,16 @@ NoUnusedVars::NoUnusedVars() : Rule("no-unused-vars") {}
 
 void NoUnusedVars::Exit(Context& ctx) {
   for (const auto& sym : ctx.GetFile().module->scope->symbols.Enumerate() | std::ranges::views::values) {
-    if ((sym.Flags() & (core::semantic::SymbolFlags::kFunction | core::semantic::SymbolFlags::kTemplate |
-                        core::semantic::SymbolFlags::kControl))) {
+    const auto perform_check = [&] {
       CheckScope({this, ctx}, sym.OriginatedScope(), nullptr);
+    };
+    if (sym.Flags() & core::semantic::SymbolFlags::kFunction) {
+      const auto* funcdecl = sym.Declaration()->As<core::ast::nodes::FuncDecl>();
+      if (!funcdecl->external) [[likely]] {
+        perform_check();
+      }
+    } else if (sym.Flags() & (core::semantic::SymbolFlags::kTemplate | core::semantic::SymbolFlags::kControl)) {
+      perform_check();
     }
   }
 };
