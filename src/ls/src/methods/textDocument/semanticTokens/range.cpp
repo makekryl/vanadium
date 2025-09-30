ca2444734b8c3ca6456eed3ce5665c61b240acda
makekryl <makekryl@gmail.com>
2025-09-30 20:41:30 +0000

    cleanup lsp range semtok a bit

--- src/ls/src/methods/textDocument/semanticTokens/range.cpp
+++ src/ls/src/methods/textDocument/semanticTokens/range.cpp
@@ -1,3 +1,4 @@
+#include <concepts>
 #include <utility>
 
 #include "ASTNodes.h"
@@ -7,10 +8,12 @@
 #include "LanguageServerConv.h"
 #include "LanguageServerLogger.h"
 #include "LanguageServerMethods.h"
+#include "Program.h"
 #include "ScopedNodeVisitor.h"
 #include "Semantic.h"
 #include "TypeChecker.h"
 #include "detail/InlayHint.h"
+#include "utils/ASTUtils.h"
 
 namespace lsp {
 inline SemanticTokenModifiers operator|(SemanticTokenModifiers lhs, SemanticTokenModifiers rhs) {
@@ -26,6 +29,98 @@ struct SemanticTokenW {
   lsp::SemanticTokenTypes type;
   lsp::SemanticTokenModifiers modifiers;
 };
+
+void HighlightIdent(const core::SourceFile* file, const core::semantic::Scope* scope,
+                    const core::ast::nodes::Ident* ident, std::invocable<SemanticTokenW&&> auto write_token) {
+  if (ident->parent->nkind == core::ast::NodeKind::AssignmentExpr) {
+    const auto* ae = ident->parent->As<core::ast::nodes::AssignmentExpr>();
+    if (ident == ae->property && ae->parent->nkind == core::ast::NodeKind::CompositeLiteral) {
+      write_token(SemanticTokenW{
+          .ident = ident,
+          .type = lsp::SemanticTokenTypes::kProperty,
+          .modifiers = lsp::SemanticTokenModifiers::kUnset,
+      });
+      return;
+    }
+  }
+  if (ident->parent->nkind == core::ast::NodeKind::EnumTypeDecl ||
+      (ident->parent->nkind == core::ast::NodeKind::CallExpr &&
+       ident->parent->parent->nkind == core::ast::NodeKind::EnumTypeDecl)) {
+    write_token(SemanticTokenW{
+        .ident = ident,
+        .type = lsp::SemanticTokenTypes::kEnummember,
+        .modifiers = lsp::SemanticTokenModifiers::kUnset,
+    });
+    return;
+  }
+  if (const auto* sym = core::checker::ResolveExprSymbol(file, scope, ident); sym) {
+    SemanticTokenW w{.ident = ident};
+    using Fu = std::pair<lsp::SemanticTokenTypes, lsp::SemanticTokenModifiers>;
+    std::tie(w.type, w.modifiers) = [&] -> Fu {
+      if (sym->Flags() & core::semantic::SymbolFlags::kTemplate) {
+        return {lsp::SemanticTokenTypes::kVariable,
+                (ident->parent->nkind == core::ast::NodeKind::FuncDecl ? lsp::SemanticTokenModifiers::kDeclaration
+                                                                       : lsp::SemanticTokenModifiers::kUnset) |
+                    lsp::SemanticTokenModifiers::kReadonly};
+      }
+      if (sym->Flags() & core::semantic::SymbolFlags::kFunction) {
+        return {lsp::SemanticTokenTypes::kFunction,
+                (ident->parent->nkind == core::ast::NodeKind::FuncDecl ? lsp::SemanticTokenModifiers::kDeclaration
+                                                                       : lsp::SemanticTokenModifiers::kUnset)};
+      }
+      if (sym->Flags() & core::semantic::SymbolFlags::kVariable) {
+        const auto* decl = sym->Declaration();
+        if (decl->nkind == core::ast::NodeKind::Declarator) {
+          const auto* dd = decl->As<core::ast::nodes::Declarator>();
+          const auto* vd = dd->parent->As<core::ast::nodes::ValueDecl>();
+          if (vd->kind && vd->kind->kind != core::ast::TokenKind::VAR) {  // modulepar+const, todo: refactor
+            return {lsp::SemanticTokenTypes::kVariable,
+                    lsp::SemanticTokenModifiers::kDefinition | lsp::SemanticTokenModifiers::kReadonly};
+          }
+          return {lsp::SemanticTokenTypes::kVariable, lsp::SemanticTokenModifiers::kDeclaration};
+        }
+        return {lsp::SemanticTokenTypes::kVariable, lsp::SemanticTokenModifiers::kUnset};
+      }
+      if (sym->Flags() & core::semantic::SymbolFlags::kArgument) {
+        return {lsp::SemanticTokenTypes::kParameter,
+                (ident->parent->nkind == core::ast::NodeKind::FormalPar ? lsp::SemanticTokenModifiers::kDeclaration
+                                                                        : lsp::SemanticTokenModifiers::kUnset)};
+      }
+      if (sym->Flags() & (core::semantic::SymbolFlags::kComponent | core::semantic::SymbolFlags::kClass)) {
+        return {lsp::SemanticTokenTypes::kClass, lsp::SemanticTokenModifiers::kUnset};
+      }
+      if (sym->Flags() & (core::semantic::SymbolFlags::kEnum)) {
+        return {lsp::SemanticTokenTypes::kEnum, lsp::SemanticTokenModifiers::kUnset};
+      }
+      if (sym->Flags() & (core::semantic::SymbolFlags::kEnumMember)) {
+        return {lsp::SemanticTokenTypes::kEnummember, lsp::SemanticTokenModifiers::kUnset};
+      }
+      if (sym->Flags() & (core::semantic::SymbolFlags::kStructural)) {
+        return {lsp::SemanticTokenTypes::kStruct, lsp::SemanticTokenModifiers::kUnset};
+      }
+      if (sym->Flags() & core::semantic::SymbolFlags::kField) {
+        return {lsp::SemanticTokenTypes::kType, lsp::SemanticTokenModifiers::kDeclaration};
+      }
+      if (sym->Flags() & core::semantic::SymbolFlags::kSubtype) {
+        return {lsp::SemanticTokenTypes::kType, lsp::SemanticTokenModifiers::kUnset};
+      }
+      if (sym->Flags() & core::semantic::SymbolFlags::kPort) {
+        return {lsp::SemanticTokenTypes::kClass, lsp::SemanticTokenModifiers::kUnset};
+      }
+      if (sym->Flags() & core::semantic::SymbolFlags::kType) {
+        return {lsp::SemanticTokenTypes::kType, lsp::SemanticTokenModifiers::kUnset};
+      }
+
+      // regexp lol todo
+      return {lsp::SemanticTokenTypes::kRegexp, lsp::SemanticTokenModifiers::kUnset};
+    }();
+    // regexp lol todo
+    if (w.type != lsp::SemanticTokenTypes::kRegexp) {
+      write_token(std::move(w));
+    }
+  }
+}
+
 lsp::SemanticTokens CollectSemanticTokens(const lsp::SemanticTokensRangeParams& params, const core::SourceFile& file,
                                           LsSessionRef) {
   if (!file.module) {
@@ -56,110 +151,33 @@ lsp::SemanticTokens CollectSemanticTokens(const lsp::SemanticTokensRangeParams& 
   const auto overlaps = [](const core::ast::Range& a, const core::ast::Range& b) {
     return std::max(a.begin, b.begin) <= std::min(a.end, b.end);
   };
+
   const core::semantic::Scope* scope{nullptr};
+  const auto inspector = [&](this auto&& self, const core::ast::Node* n) -> bool {
+    if (overlaps(requested_range, n->nrange)) {
+      switch (n->nkind) {
+        case core::ast::NodeKind::Ident: {
+          HighlightIdent(&file, scope, n->As<core::ast::nodes::Ident>(), write_token);
+          return false;
+        }
+        case core::ast::NodeKind::SelectorExpr: {
+          core::ast::utils::TraverseSelectorExpressionStart(n->As<core::ast::nodes::SelectorExpr>())->Accept(self);
+          return false;
+        }
+        default: {
+          break;
+        }
+      }
+      return true;
+    }
+    return n->nrange.Contains(requested_range);
+  };
   core::semantic::InspectScope(
       file.module->scope,
       [&](const core::semantic::Scope* scope_under_inspection) {
         scope = scope_under_inspection;
       },
-      [&](const core::ast::Node* n) -> bool {
-        if (overlaps(requested_range, n->nrange)) {
-          if (n->nkind == core::ast::NodeKind::Ident) {
-            const auto* ident = n->As<core::ast::nodes::Ident>();
-            if (ident->parent->nkind == core::ast::NodeKind::AssignmentExpr) {
-              const auto* ae = ident->parent->As<core::ast::nodes::AssignmentExpr>();
-              if (ident == ae->property && ae->parent->nkind == core::ast::NodeKind::CompositeLiteral) {
-                write_token(SemanticTokenW{
-                    .ident = ident,
-                    .type = lsp::SemanticTokenTypes::kProperty,
-                    .modifiers = lsp::SemanticTokenModifiers::kUnset,
-                });
-                return true;
-              }
-            }
-            if (ident->parent->nkind == core::ast::NodeKind::EnumTypeDecl ||
-                (ident->parent->nkind == core::ast::NodeKind::CallExpr &&
-                 ident->parent->parent->nkind == core::ast::NodeKind::EnumTypeDecl)) {
-              write_token(SemanticTokenW{
-                  .ident = ident,
-                  .type = lsp::SemanticTokenTypes::kEnummember,
-                  .modifiers = lsp::SemanticTokenModifiers::kUnset,
-              });
-              return true;
-            }
-            if (const auto* sym = core::checker::ResolveExprSymbol(&file, scope, ident); sym) {
-              SemanticTokenW w{.ident = ident};
-              using Fu = std::pair<lsp::SemanticTokenTypes, lsp::SemanticTokenModifiers>;
-              std::tie(w.type, w.modifiers) = [&] -> Fu {
-                if (sym->Flags() & core::semantic::SymbolFlags::kTemplate) {
-                  return {lsp::SemanticTokenTypes::kVariable, (ident->parent->nkind == core::ast::NodeKind::FuncDecl
-                                                                   ? lsp::SemanticTokenModifiers::kDeclaration
-                                                                   : lsp::SemanticTokenModifiers::kUnset) |
-                                                                  lsp::SemanticTokenModifiers::kReadonly};
-                }
-                if (sym->Flags() & core::semantic::SymbolFlags::kFunction) {
-                  return {lsp::SemanticTokenTypes::kFunction, (ident->parent->nkind == core::ast::NodeKind::FuncDecl
-                                                                   ? lsp::SemanticTokenModifiers::kDeclaration
-                                                                   : lsp::SemanticTokenModifiers::kUnset)};
-                }
-                if (sym->Flags() & core::semantic::SymbolFlags::kVariable) {
-                  const auto* decl = sym->Declaration();
-                  if (decl->nkind == core::ast::NodeKind::Declarator) {
-                    const auto* dd = decl->As<core::ast::nodes::Declarator>();
-                    const auto* vd = dd->parent->As<core::ast::nodes::ValueDecl>();
-                    if (vd->kind && vd->kind->kind != core::ast::TokenKind::VAR) {  // modulepar+const, todo: refactor
-                      return {lsp::SemanticTokenTypes::kVariable,
-                              lsp::SemanticTokenModifiers::kDefinition | lsp::SemanticTokenModifiers::kReadonly};
-                    }
-                    return {lsp::SemanticTokenTypes::kVariable, lsp::SemanticTokenModifiers::kDeclaration};
-                  }
-                  return {lsp::SemanticTokenTypes::kVariable, lsp::SemanticTokenModifiers::kUnset};
-                }
-                if (sym->Flags() & core::semantic::SymbolFlags::kArgument) {
-                  return {lsp::SemanticTokenTypes::kParameter, (ident->parent->nkind == core::ast::NodeKind::FormalPar
-                                                                    ? lsp::SemanticTokenModifiers::kDeclaration
-                                                                    : lsp::SemanticTokenModifiers::kUnset)};
-                }
-                if (sym->Flags() & (core::semantic::SymbolFlags::kComponent | core::semantic::SymbolFlags::kClass)) {
-                  return {lsp::SemanticTokenTypes::kClass, lsp::SemanticTokenModifiers::kUnset};
-                }
-                if (sym->Flags() & (core::semantic::SymbolFlags::kEnum)) {
-                  return {lsp::SemanticTokenTypes::kEnum, lsp::SemanticTokenModifiers::kUnset};
-                }
-                if (sym->Flags() & (core::semantic::SymbolFlags::kEnumMember)) {
-                  return {lsp::SemanticTokenTypes::kEnummember, lsp::SemanticTokenModifiers::kUnset};
-                }
-                if (sym->Flags() & (core::semantic::SymbolFlags::kStructural)) {
-                  return {lsp::SemanticTokenTypes::kStruct, lsp::SemanticTokenModifiers::kUnset};
-                }
-                if (sym->Flags() & core::semantic::SymbolFlags::kField) {
-                  return {lsp::SemanticTokenTypes::kType, lsp::SemanticTokenModifiers::kDeclaration};
-                }
-                if (sym->Flags() & core::semantic::SymbolFlags::kSubtype) {
-                  return {lsp::SemanticTokenTypes::kType, lsp::SemanticTokenModifiers::kUnset};
-                }
-                if (sym->Flags() & core::semantic::SymbolFlags::kPort) {
-                  return {lsp::SemanticTokenTypes::kClass, lsp::SemanticTokenModifiers::kUnset};
-                }
-                if (sym->Flags() & core::semantic::SymbolFlags::kType) {
-                  return {lsp::SemanticTokenTypes::kType, lsp::SemanticTokenModifiers::kUnset};
-                }
-
-                // regexp lol todo
-                return {lsp::SemanticTokenTypes::kRegexp, lsp::SemanticTokenModifiers::kUnset};
-              }();
-              // regexp lol todo
-              if (w.type != lsp::SemanticTokenTypes::kRegexp) {
-                write_token(std::move(w));
-              }
-            }
-
-            return true;
-          }
-          return true;
-        }
-        return n->nrange.Contains(requested_range);
-      });
+      inspector);
 
   return result;
 }
