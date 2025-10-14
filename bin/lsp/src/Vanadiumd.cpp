62dcfa16ec8a343cb034f1769bd85c3f4b3e06a3
makekryl <makekryl@gmail.com>
2025-10-14 17:29:39 +0000

    vanadiumd: rename opts

--- bin/lsp/src/Vanadiumd.cpp
+++ bin/lsp/src/Vanadiumd.cpp
@@ -46,7 +46,7 @@ int main(int argc, char* argv[]) {
   ap.add_description("TTCN-3 language server");
   //
   std::uint32_t jobs{std::clamp(std::thread::hardware_concurrency(), 1U, 4U)};
-  ap.add_argument("-j", "--parallel", "").store_into(jobs).help("maximum number of worker threads");
+  ap.add_argument("-j", "--jobs", "").store_into(jobs).help("maximum number of worker threads");
   std::uint32_t concurrency{1};  // >1 is not supported at the moment
   ap.add_argument("--concurrency").store_into(concurrency).help("number of concurrently served LS requests");
   //
@@ -68,7 +68,7 @@ int main(int argc, char* argv[]) {
   }
   /////////////////////////////////////////////
 
-  if (ap["--wait-dbg"] == true) {  // == true, yes
+  if (ap.get<bool>("--wait-dbg")) {
     constexpr auto kSecondsToWait{12};
     std::println(stderr, "\nWaiting {} seconds for debugger to attach...\n", kSecondsToWait);
     std::this_thread::sleep_for(std::chrono::seconds(kSecondsToWait));
@@ -80,7 +80,7 @@ int main(int argc, char* argv[]) {
   vanadium::lserver::Transport* transport{&stdio_transport};
 
   std::optional<OutputCapturingTransport> capturing_transport;
-  if (ap["--capture-output"] == true) {
+  if (ap.get<bool>("--capture-output")) {
     transport = &capturing_transport.emplace("vanadiumd.stdout.log", *transport);
   }
 
