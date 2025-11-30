27c16d63be1b633fe421e7044b3f44e9ee4ac77b
makekryl <makekryl@gmail.com>
2025-11-30 13:02:47 +0000

    update glaze, fix clang build

--- cmake/external/glaze.cmake
+++ cmake/external/glaze.cmake
@@ -1,7 +1,8 @@
 FetchContent_Declare(
-  glaze          # 5.5.5
-  GIT_REPOSITORY https://github.com/stephenberry/glaze.git
-  GIT_TAG        5c24f146326580da68047c2180e9570b9c04b82e
+  glaze          # 6.1.0
+  # GIT_REPOSITORY https://github.com/stephenberry/glaze.git
+  GIT_REPOSITORY https://github.com/makekryl/glaze.git
+  GIT_TAG        eba6885609d4371e0d527ba5af0fd0ccca882541
   GIT_SHALLOW 1
 )
 FetchContent_MakeAvailable(glaze)
