452c316edc59b46498de441e288650fdc7ca8f6b
makekryl <makekryl@gmail.com>
2025-12-01 19:42:29 +0000

    add todo

--- cmake/external/glaze.cmake
+++ cmake/external/glaze.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
   glaze          # 6.1.0
-  # GIT_REPOSITORY https://github.com/stephenberry/glaze.git
+  # GIT_REPOSITORY https://github.com/stephenberry/glaze.git  # TODO: https://github.com/stephenberry/glaze/pull/2101
   GIT_REPOSITORY https://github.com/makekryl/glaze.git
   GIT_TAG        eba6885609d4371e0d527ba5af0fd0ccca882541
   GIT_SHALLOW 1
