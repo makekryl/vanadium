26c69e9e18d39ac31db3a903e4368bbb04e9341a
makekryl <makekryl@gmail.com>
2025-12-24 19:42:44 +0000

    vendor asn1c/libasn1parser

---
+++ lib/libasn1parser/include/asn1c/libasn1parser/asn1parser_cc.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wc99-designator"
+#pragma GCC diagnostic ignored "-Wwritable-strings"
+
+extern "C" {
+#include "asn1parser.h"  // IWYU pragma: keep
+}
+
+#pragma GCC diagnostic pop
