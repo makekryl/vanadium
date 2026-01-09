551222d598f033f909e0366cd518087c45568b10
makekryl <makekryl@gmail.com>
2026-01-09 15:02:09 +0000

    fmt

--- lib/asn1c_libasn1parser/include/asn1c/libasn1parser/asn1parser_cxx.h
+++ lib/asn1c_libasn1parser/include/asn1c/libasn1parser/asn1parser_cxx.h
@@ -12,14 +12,14 @@ extern "C" {
 #include "asn1p_alloc.h"                     // IWYU pragma: export
 #include "asn1p_integer.h"                   // IWYU pragma: export
 #include "asn1p_list.h"                      // IWYU pragma: export
-#include "asn1p_oid.h"		                   // IWYU pragma: export
-#include "asn1p_module.h"	                   // IWYU pragma: export
-#include "asn1p_value.h"	                   // IWYU pragma: export
-#include "asn1p_param.h"	                   // IWYU pragma: export
-#include "asn1p_constr.h"	                   // IWYU pragma: export
-#include "asn1p_xports.h"	                   // IWYU pragma: export
-#include "asn1p_class.h"	                   // IWYU pragma: export
-#include "asn1p_expr.h"		                   // IWYU pragma: export
+#include "asn1p_oid.h"                       // IWYU pragma: export
+#include "asn1p_module.h"                    // IWYU pragma: export
+#include "asn1p_value.h"                     // IWYU pragma: export
+#include "asn1p_param.h"                     // IWYU pragma: export
+#include "asn1p_constr.h"                    // IWYU pragma: export
+#include "asn1p_xports.h"                    // IWYU pragma: export
+#include "asn1p_class.h"                     // IWYU pragma: export
+#include "asn1p_expr.h"                      // IWYU pragma: export
 // clang-format on
 }
 
