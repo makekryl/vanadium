4834717324ef474bd6208c6e3b5d0e54f923b6ae
makekryl <makekryl@gmail.com>
2026-02-02 19:13:27 +0000

    deps: update glaze to 7.0.2

--- cmake/external/glaze.cmake
+++ cmake/external/glaze.cmake
@@ -1,8 +1,15 @@
-FetchContent_Declare(
-  glaze          # 6.1.0
-  # GIT_REPOSITORY https://github.com/stephenberry/glaze.git  # TODO: https://github.com/stephenberry/glaze/pull/2101
-  GIT_REPOSITORY https://github.com/makekryl/glaze.git
-  GIT_TAG        eba6885609d4371e0d527ba5af0fd0ccca882541
-  GIT_SHALLOW 1
+set(GLAZE_SOURCE_PATCH_COMMAND git apply
+  # https://github.com/stephenberry/glaze/pull/2101
+  ${CMAKE_CURRENT_LIST_DIR}/patches/glaze-001-fix-clang19-variant-build.patch
 )
+
+FetchContent_Declare(
+  glaze          # 7.0.2
+  GIT_REPOSITORY https://github.com/stephenberry/glaze.git
+  GIT_TAG        459946d325c497c274fa59d666bd7cb6e2dd7ad0
+  GIT_SHALLOW 1
+
+  PATCH_COMMAND ${GLAZE_SOURCE_PATCH_COMMAND}
+)
+
 FetchContent_MakeAvailable(glaze)
