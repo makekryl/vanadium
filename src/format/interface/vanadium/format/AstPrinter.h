71959a58cd235fc1e1d0249680275657891e46e0
makekryl <makekryl@gmail.com>
2026-02-15 18:46:19 +0000

    fmt: complete pretty-printer

--- src/format/interface/vanadium/format/AstPrinter.h
+++ src/format/interface/vanadium/format/AstPrinter.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include <string>
 
 namespace vanadium {
@@ -14,6 +15,8 @@ namespace format {
 struct PrintOptions {
   std::size_t tab_width;
   std::size_t print_width;
+
+  std::size_t max_newlines{3};
 };
 
 std::string PrintAst(const ast::AST& ast, const ast::Node*, PrintOptions);
--- src/format/src/AstPrinter.cpp
+++ src/format/src/AstPrinter.cpp
@@ -1,6 +1,7 @@
 #include "vanadium/format/AstPrinter.h"
 
 #include <cassert>
+#include <ranges>
 #include <string_view>
 #include <utility>
 
@@ -13,25 +14,50 @@
 namespace vanadium::format {
 
 namespace {
+
+bool IsLineComment(const Comment& comment) {
+  return comment.content.starts_with("//");
+}
+
 class SerialAstPrinter {
  public:
   SerialAstPrinter(std::string_view src, PrintOptions options) : src_(src), options_(std::move(options)) {}
 
   std::string Print(const Unit& u) {
-    AppendUnit(u);
+    AppendUnit(u, Wrap::kAuto);
     return std::move(buf_);
   }
 
  private:
-  void AppendUnit(const Unit&);
+  enum class Wrap : std::uint8_t {
+    kAuto,
+    kEnabled,
+  };
 
+  [[nodiscard]] std::size_t Width(const Sequence&);
+  void AppendUnit(const Unit&, Wrap);
+
+  std::size_t consecutive_newlines_{0};
   void WriteNewline() {
     buf_ += "\n";
-    line_length_ = 0;
+
+    const auto sl = (indent_ * options_.tab_width);
+    for (std::size_t i = 0; i < sl; ++i) {
+      buf_ += ' ';
+    }
+    line_length_ = sl;
+
+    ++consecutive_newlines_;
   }
   void Write(std::string_view s) {
     buf_ += s;
     line_length_ += s.length();
+
+    consecutive_newlines_ = 0;
+  }
+
+  [[nodiscard]] bool IsCurrentLineEmpty() const {
+    return consecutive_newlines_ > 0;
   }
 
   std::string buf_;
@@ -43,15 +69,64 @@ class SerialAstPrinter {
   const PrintOptions options_;
 };
 
-void SerialAstPrinter::AppendUnit(const Unit& cu) {
+std::size_t SerialAstPrinter::Width(const Sequence& seq) {
+  const auto visitor = mp::Overloaded{
+      [&](const Sequence* seq) {
+        return Width(*seq);
+      },
+      [&](PrintDirective pd) {
+        switch (pd) {
+          case PrintDirective::kSpace:
+          case PrintDirective::kSpaceOrLine:
+            return 1UL;
+          case PrintDirective::kHardLine:
+            return options_.print_width;
+          case PrintDirective::kSoftLine:
+            return 0UL;
+          case PrintDirective::kSemicolon:
+            return 1UL;
+        }
+      },
+      [&](std::string_view text) {
+        return text.length();
+      },
+      [&](const Comment& comment) {
+        return IsLineComment(comment) ? options_.print_width : comment.content.length();
+      },
+      [&](const PreferredNewlines&) {
+        return options_.print_width;
+      },
+      [&](EmptyUnit) {
+        return 0UL;
+      },
+  };
+
+  std::size_t width = 0;
+  for (const auto& u : seq.units) {
+    width += std::visit(visitor, u);
+  }
+  return width;
+}
+
+void SerialAstPrinter::AppendUnit(const Unit& cu, Wrap wrap) {
   const auto visitor = mp::Overloaded{
       [&](const Sequence* seq) -> void {
         if (seq->attributes & Sequence::Attribute::kIndented) {
           ++indent_;
         }
-        for (const auto& u : seq->units) {
-          AppendUnit(u);
+
+        Wrap children_wrap = wrap;
+        if (children_wrap == Wrap::kAuto && (seq->attributes & Sequence::Attribute::kGrouped)) {
+          const auto width = Width(*seq);
+          if (width > options_.print_width) {
+            children_wrap = Wrap::kEnabled;
+          }
         }
+
+        for (const auto& u : seq->units) {
+          AppendUnit(u, children_wrap);
+        }
+
         if (seq->attributes & Sequence::Attribute::kIndented) {
           --indent_;
         }
@@ -59,11 +134,15 @@ void SerialAstPrinter::AppendUnit(const Unit& cu) {
       [&](PrintDirective pd) -> void {
         switch (pd) {
           case PrintDirective::kSpace: {
-            buf_ += " ";
+            Write(" ");
             break;
           }
           case PrintDirective::kSpaceOrLine: {
-            buf_ += " ";
+            if (wrap == Wrap::kEnabled) {
+              WriteNewline();
+            } else {
+              Write(" ");
+            }
             break;
           }
           case PrintDirective::kHardLine: {
@@ -71,36 +150,58 @@ void SerialAstPrinter::AppendUnit(const Unit& cu) {
             break;
           }
           case PrintDirective::kSoftLine: {
-            // WriteNewline();
+            if (wrap == Wrap::kEnabled) {
+              WriteNewline();
+            }
             break;
           }
           case PrintDirective::kSemicolon: {
-            buf_ += ";";
-            break;
-          }
-          default: {
-            assert(false && "Unhandled PrintDirective");
+            Write(";");
             break;
           }
         }
       },
       [&](std::string_view text) -> void {
-        if (line_length_ < indent_ * options_.tab_width) {
-          const auto sl = (indent_ * options_.tab_width) - line_length_;
-          for (std::size_t i = 0; i < sl; ++i) {
-            buf_ += ' ';
-          }
-          line_length_ += sl;
-        }
-        buf_ += text;
+        Write(text);
       },
       [&](const Comment& comment) -> void {
-        // todo: force wrap
-        buf_ += comment.content;
+        if (IsLineComment(comment)) {
+          if (!IsCurrentLineEmpty()) {
+            Write("  ");
+          }
+          Write(comment.content);
+        } else {
+          if (!IsCurrentLineEmpty()) {
+            Write(" ");
+          }
+          auto lines = comment.content | std::views::split('\n');
+          auto it = lines.begin();
+          auto end = lines.end();
+          while (it != end) {
+            std::string_view line{*(it++)};
+            while (!line.empty() && std::isspace(line.front())) {
+              line.remove_prefix(1);
+            }
+            while (!line.empty() && std::isspace(line.back())) {
+              line.remove_suffix(1);
+            }
+            if (line.starts_with('*')) {
+              Write(" ");
+            }
+            Write(line);
+            if (it != end) {
+              WriteNewline();
+            }
+          }
+        }
       },
       [&](const PreferredNewlines& pnl) -> void {
-        // todo: force wrap
-        for (std::size_t i = 0; i < pnl.count; ++i) {
+        auto count = pnl.count;
+        if (IsCurrentLineEmpty()) {
+          --count;
+        }
+        count = std::min(count, options_.max_newlines);
+        for (std::size_t i = 0; i < count; ++i) {
           WriteNewline();
         }
       },
