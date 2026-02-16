bc8a08efc661237d3ce98613c0705df640007f64
makekryl <makekryl@gmail.com>
2026-02-16 23:08:29 +0000

    fmt: add basic arguments to the executable

--- bin/fmt/src/VanadiumFormatter.cpp
+++ bin/fmt/src/VanadiumFormatter.cpp
@@ -15,6 +15,11 @@
 using namespace vanadium;
 
 namespace {
+std::string ReadStdin() {
+  std::ostringstream ss;
+  ss << std::cin.rdbuf();
+  return ss.str();
+}
 std::optional<std::string> ReadFile(const std::string& path) {
   // TODO: check & raise errors
   if (auto f = std::ifstream(path)) {
@@ -23,6 +28,12 @@ std::optional<std::string> ReadFile(const std::string& path) {
     return buf.str();
   }
   return std::nullopt;
+}
+void WriteFile(const std::string& path, std::string_view content) {
+  // TODO: check & raise errors
+  if (auto f = std::ofstream(path, std::ios::out | std::ios::trunc)) {
+    f << content;
+  }
 }
 }  // namespace
 
@@ -34,6 +45,14 @@ int main(int argc, char* argv[]) {
   std::string filepath;
   ap.add_argument("path").store_into(filepath).help("file path");
   //
+  bool use_dryrun{false};
+  bool use_write{false};
+  {
+    auto& group = ap.add_mutually_exclusive_group();
+    group.add_argument("--dryrun", "-n").store_into(use_dryrun).help("dry-run mode");
+    group.add_argument("--write", "-w").store_into(use_write).help("overwrite file");
+  }
+  //
   bool just_dump_tree{false};
   ap.add_argument("--dump-tree").store_into(just_dump_tree).help("dump serialized AST tree");
 
@@ -41,14 +60,24 @@ int main(int argc, char* argv[]) {
   PARSE_CLI_ARGS_OR_EXIT(ap, argc, argv, 1);
   //
 
-  auto src = ReadFile(filepath);
+  const bool read_from_stdin = filepath == "-";
+
+  auto src = read_from_stdin ? ReadStdin() : ReadFile(filepath);
   if (!src) {
-    std::println(stderr, "Failed to open '{}'", filepath);
-    return 1;
+    std::println(stderr, "Failed to read '{}'", filepath);
+    return 3;
   }
 
   lib::Arena arena;
-  auto ast = ast::Parse(arena, *src);
+  const auto ast = ast::Parse(arena, *src);
+
+  if (!ast.errors.empty()) {
+    std::println(stderr, "File has syntax errors:", filepath);
+    for (const auto& err : ast.errors) {
+      std::println(stderr, " - ({}:{}): {}", err.range.begin, err.range.end, err.description);
+    }
+    return 2;
+  }
 
   if (just_dump_tree) {
     std::ios_base::sync_with_stdio(false);
@@ -57,11 +86,25 @@ int main(int argc, char* argv[]) {
     return 0;
   }
 
-  std::println("{}", format::PrintAst(ast, ast.root,
-                                      format::PrintOptions{
-                                          .tab_width = 4,
-                                          .print_width = 80,
-                                      }));
+  const auto result = format::PrintAst(ast, ast.root,
+                                       format::PrintOptions{
+                                           .tab_width = 4,
+                                           .print_width = 80,
+                                       });
+  if (use_dryrun) {
+    const bool no_diffs = ast.src == result;
+    return no_diffs ? 0 : 1;
+  }
+
+  if (!use_write) {
+    std::cout << result;
+  } else {
+    if (read_from_stdin) {
+      std::println(stderr, "Cannot use write-mode with stdin as input file path");
+      return 4;
+    }
+    WriteFile(filepath, result);
+  }
 
   return 0;
 }
