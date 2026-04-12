379c8b38a67153b2e000cbc790210571372655ed
makekryl <makekryl@gmail.com>
2026-04-12 23:28:09 +0000

    Fix semicolon in macro

--- lib/common/include/vanadium/lib/Assert.h
+++ lib/common/include/vanadium/lib/Assert.h
@@ -8,14 +8,14 @@
     std::println(stderr, __FILE__ ":{} :: " msg, __LINE__, ##__VA_ARGS__); \
     std::fflush(stderr);                                                   \
     std::exit(42);                                                         \
-  } while (0);
+  } while (0)
 
 #define VANADIUM_DEBUG_ASSERT(cond, msg, ...)   \
   do {                                          \
     if (!(cond)) {                              \
       VANADIUM_DEBUG_ERROR(msg, ##__VA_ARGS__); \
     }                                           \
-  } while (0);
+  } while (0)
 
 #else
 #define VANADIUM_DEBUG_ERROR(...)
