9ba9de1dac398278922df0c762502da87b5f33dd
makekryl <makekryl@gmail.com>
2026-04-23 20:43:35 +0000

    lint: cleanup NoUnusedVars

--- src/lint/src/rules/NoUnusedVars.cpp
+++ src/lint/src/rules/NoUnusedVars.cpp
@@ -28,80 +28,82 @@ ast::Range GetVariableNameRange(const ast::Node* decl) {
   }
 }
 
-void CheckScope(const std::pair<NoUnusedVars*, Context&> rule_ctx, const core::semantic::Scope* scope,
-                std::unordered_set<std::string_view>* outer_used_vars) {
-  const auto& [rule, ctx] = rule_ctx;
+struct ScopeChecker {
+  const NoUnusedVars* rule;
+  Context& ctx;
 
-  std::unordered_set<std::string_view> used_vars;
-  for (auto* child : scope->GetChildren()) {
-    CheckScope(rule_ctx, child, &used_vars);
-  }
+  void Check(const core::semantic::Scope* scope, std::unordered_set<std::string_view>* outer_used_vars) {
+    std::unordered_set<std::string_view> used_vars;
+    for (auto* child : scope->GetChildren()) {
+      Check(child, &used_vars);
+    }
 
-  const auto inspector = [&](this auto&& self, const ast::Node* n) -> bool {
-    switch (n->nkind) {
-      case ast::NodeKind::FormalPars: {
-        return false;
-      }
-      case ast::NodeKind::SelectorExpr: {
-        const auto* se = n->As<ast::nodes::SelectorExpr>();
-        ast::Inspect(ast::utils::TraverseSelectorExpressionStart(se), self);
-        return false;
-      }
-      case ast::NodeKind::Declarator: {
-        auto* val = n->As<ast::nodes::Declarator>()->value;
-        if (val != nullptr) {
-          ast::Inspect(val, self);
-        }
-        return false;
-      }
-      case ast::NodeKind::AssignmentExpr: {
-        const auto* ae = n->As<ast::nodes::AssignmentExpr>();
-        if (ae->parent->nkind == ast::NodeKind::CompositeLiteral || ae->parent->nkind == ast::NodeKind::ParenExpr) {
-          ast::Inspect(ae->value, self);
+    const auto inspector = [&](this auto&& self, const ast::Node* n) -> bool {
+      switch (n->nkind) {
+        case ast::NodeKind::FormalPars: {
           return false;
         }
-        return true;
+        case ast::NodeKind::SelectorExpr: {
+          const auto* se = n->As<ast::nodes::SelectorExpr>();
+          ast::Inspect(ast::utils::TraverseSelectorExpressionStart(se), self);
+          return false;
+        }
+        case ast::NodeKind::Declarator: {
+          auto* val = n->As<ast::nodes::Declarator>()->value;
+          if (val != nullptr) {
+            ast::Inspect(val, self);
+          }
+          return false;
+        }
+        case ast::NodeKind::AssignmentExpr: {
+          const auto* ae = n->As<ast::nodes::AssignmentExpr>();
+          if (ae->parent->nkind == ast::NodeKind::CompositeLiteral || ae->parent->nkind == ast::NodeKind::ParenExpr) {
+            ast::Inspect(ae->value, self);
+            return false;
+          }
+          return true;
+        }
+        case ast::NodeKind::Ident: {
+          used_vars.emplace(ctx.GetFile().Text(n));
+          return false;
+        }
+        default: {
+          return true;
+        }
       }
-      case ast::NodeKind::Ident: {
-        used_vars.emplace(ctx.GetFile().Text(n));
-        return false;
+    };
+
+    const auto* container = scope->Container();
+    if (container != nullptr) {
+      container->Accept(inspector);
+    }
+
+    for (const auto& sym : scope->symbols.Enumerate() | std::ranges::views::values) {
+      const bool is_variable = (sym.Flags() & core::semantic::SymbolFlags::kVariable) != 0;
+      const bool is_argument = (sym.Flags() & core::semantic::SymbolFlags::kArgument) != 0;
+      if (!(is_variable || is_argument)) {
+        continue;
       }
-      default: {
-        return true;
+      const auto& name = sym.GetName();
+      if (!used_vars.contains(name)) {
+        ctx.Report(rule, GetVariableNameRange(sym.Declaration()),
+                   std::format("{} '{}' is not used", is_argument ? "argument" : "variable", name));
+      } else {
+        used_vars.erase(name);
       }
     }
-  };
 
-  const auto* container = scope->Container();
-  if (container != nullptr) {
-    container->Accept(inspector);
-  }
-
-  for (const auto& sym : scope->symbols.Enumerate() | std::ranges::views::values) {
-    const bool is_variable = (sym.Flags() & core::semantic::SymbolFlags::kVariable) != 0;
-    const bool is_argument = (sym.Flags() & core::semantic::SymbolFlags::kArgument) != 0;
-    if (!(is_variable || is_argument)) {
-      continue;
-    }
-    const auto& name = sym.GetName();
-    if (!used_vars.contains(name)) {
-      ctx.Report(rule, GetVariableNameRange(sym.Declaration()),
-                 std::format("{} '{}' is not used", is_argument ? "argument" : "variable", name));
-    } else {
-      used_vars.erase(name);
+    if (outer_used_vars) {
+      outer_used_vars->merge(used_vars);
     }
   }
-
-  if (outer_used_vars) {
-    outer_used_vars->merge(used_vars);
-  }
-}
+};
 }  // namespace
 
 void NoUnusedVars::Exit(Context& ctx) {
   for (const auto& sym : ctx.GetFile().module->scope->symbols.Enumerate() | std::ranges::views::values) {
     const auto perform_check = [&] {
-      CheckScope({this, ctx}, sym.OriginatedScope(), nullptr);
+      ScopeChecker{.rule = this, .ctx = ctx}.Check(sym.OriginatedScope(), nullptr);
     };
     if (sym.Flags() & core::semantic::SymbolFlags::kFunction) {
       const auto* funcdecl = sym.Declaration()->As<ast::nodes::FuncDecl>();
